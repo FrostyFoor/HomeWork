8c265b5d9df3560e9e9d3875761af93ee05531f5
vladimir vasilev <frosty_23@mail.ru>
2022-08-11 05:49:20 +0300

    Домашнее задание

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a, b;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+b = Convert.ToInt32(Console.ReadLine());
+    if (a > b)
+    {
+    Console.WriteLine("{0} наибольшее число", a);
+    }
+    else
+    {
+    Console.WriteLine("{0} наибольшее число ", b);
+    }
+Console.ReadLine();
+
+
+
+
+int a, b, c;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    Console.WriteLine("{0} наибольшее число", a);
+    else
+    Console.WriteLine("{0} наибольшее число", c);
+}
+else
+{
+    if(b > c)
+    Console.WriteLine("{0} наибольшее число", b);
+    else
+    Console.WriteLine("{0} наибольшее число", c);
+}
+Console.ReadLine();
+
+
+
+
+int n;
+Console.WriteLine("Введите число");
+n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine("Введенное число является четным");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Введенное число нечетное");
+    Console.ReadLine();
+}
+
+
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
