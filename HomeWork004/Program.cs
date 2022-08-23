b7131e0a48c477c36832ee85a5e147ddb9156874
vladimir vasilev <frosty_23@mail.ru>
2022-08-23 17:00:10 +0300

    Добавили четвертое домашнее задание.

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,97 @@
+﻿/*Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int exponentiation = Exponentiation(numberA, numberB);
+int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+Console.WriteLine(+ exponentiation);
+
+
+
+Console.WriteLine("Введите число");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int sumNum = SumNum(numbers);
+
+int SumNum(int numbers)
+{
+    
+    int counter = Convert.ToString(numbers).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numbers - numbers % 10;
+      result = result + (numbers - advance);
+      numbers = numbers / 10;
+    }
+    return result;
+}
+Console.WriteLine(+ sumNum);*/
+
+Console.WriteLine("Введите ряд чисел");
+string? seriesOfNumbers = Console.ReadLine();
+seriesOfNumbers = seriesOfNumbers + ",";   
+string RemovingSpaces (string series)
+{
+  string seriesNew = "";
+  for (int i = 0; i < series.Length; i++)
+  {
+    if (series[i] != ' ') 
+    {
+      seriesNew += series[i];
+    }
+  }
+  return seriesNew;
+}
+int[] ArrayOfNumbers(string seriesNew)
+{ 
+    int[] arrayOfNumbers = new int[1];
+    int j =0;
+    for (int i = 0; i < seriesNew.Length; i++)
+    {
+        string seriesNew1 = "";
+    while (seriesNew[i] != ',' && i < seriesNew.Length)
+    {
+      seriesNew1 += seriesNew[i];
+      i++;
+    }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);
+    if (i < seriesNew.Length-1)
+    {
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count)
+  {
+    Console.Write(coll[index]);
+    index++;
+    if (index < count)
+    {
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+string seriesNew = RemovingSpaces(seriesOfNumbers);
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
+PrintArry(arrayOfNumbers);
+
+
+
